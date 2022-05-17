06c00d34cceebb9cf0fbbd0f9d4b5a76c3ec4e93
meirson1 <noam220599@gmail.com>
2022-05-17 14:01:43 +0300

    Note: Api monthly limit 250 requests

--- src/App.js
+++ src/App.js
@@ -1,15 +1,87 @@
 import "./App.css";
 import CurrencyRow from "./CurrencyRow";
+import React, { useEffect, useState } from "react";
+
+const BASE_URL = "https://api.apilayer.com/exchangerates_data/latest?";
 
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState();
+  const [exchangeRate, setExchangeRate] = useState();
+  const [amount, setAmount] = useState(1);
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+
+  let toAmount, fromAmount;
+  if (amountInFromCurrency) {
+    fromAmount = amount;
+    toAmount = amount * exchangeRate;
+  } else {
+    toAmount = amount;
+    fromAmount = amount / exchangeRate;
+  }
+
+  var myHeaders = new Headers();
+  myHeaders.append("apikey", "4Jqt2j5bHAgkUlzHzvVpIJhE1MYR7wS0");
+
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+    headers: myHeaders,
+  };
+
+  useEffect(() => {
+    fetch(BASE_URL + "symbols=&base=", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        const firstCurrency = Object.keys(result.rates)[0];
+        setCurrencyOptions([result.base, ...Object.keys(result.rates)]);
+        setFromCurrency(result.base);
+        setToCurrency(firstCurrency);
+        setExchangeRate(result.rates[firstCurrency]);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (fromCurrency != null && toCurrency != null) {
+      fetch(
+        BASE_URL + `base=${fromCurrency}&symbols=${toCurrency}`,
+        requestOptions
+      )
+        .then((res) => res.json())
+        .then((data) => setExchangeRate(data.rates[toCurrency]));
+    }
+  }, [fromCurrency][toCurrency]);
+
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(true);
+  }
+
+  function handleToAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(false);
+  }
+
   return (
     <>
       <h1>Convert</h1>
-      <CurrencyRow />
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+        onChangeAmount={handleFromAmountChange}
+        amount={fromAmount}
+      />
       <div className="equals">=</div>
-      <CurrencyRow />
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+        onChangeCurrency={(e) => setToCurrency(e.target.value)}
+        onChangeAmount={handleToAmountChange}
+        amount={toAmount}
+      />
     </>
   );
 }
-
 export default App;
